77b63aabac1b423e9ddc63a6305c6ebd383daa59
zabellfour <zabell2814@gmail.com>
2016-11-24 23:39:38 +0200

    before pull

--- src/vendor_entries/vendor.js
+++
@@ -1,6 +0,0 @@
-module.exports = [
-  './bower/owl.carousel/dist/owl.carousel.min.js',
-  './bower/matchHeight/dist/jquery.matchHeight-min.js',
-  './bower/matchHeight/dist/jquery.matchHeight-min.js',
-  './bower/modals/dist/js/modals.js'
-];
