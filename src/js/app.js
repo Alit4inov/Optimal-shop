fbc53772589e9b652d39bfab6bab45d6025056c7
Alit4inov <alit4inov@gmail.com>
2016-11-25 21:05:42 +0200

    fix....

--- src/js/app.js
+++ src/js/app.js
@@ -8,7 +8,7 @@ jQuery(function() {
         modalActiveClass: 'active', // Class applied to active modal windows
         modalBGClass: 'modal-bg', // Class applied to the modal background overlay
         preventBGScroll: true, // Boolean, prevents background content from scroll if true
-        preventBGScrollHtml: true, // Boolean, adds overflow-y: hidden to <html> if true (preventBGScroll must also be true)
+        preventBGScrollHtml: false, // Boolean, adds overflow-y: hidden to <html> if true (preventBGScroll must also be true)
         preventBGScrollBody: true, // Boolean, adds overflow-y: hidden to <body> if true (preventBGScroll must also be true)
         backspaceClose: false, // Boolean, whether or not to enable backspace/delete button modal closing
         stopVideo: true, // Boolean, if true, stop videos when tab closes
@@ -19,39 +19,6 @@ jQuery(function() {
             $("body").toggleClass("modal-opened")
         }
     });
-});
-
-jQuery('<div class="quantity-nav"><div class="quantity-button quantity-up"></div><div class="quantity-button quantity-down"></div></div>').insertAfter('.quantity input');
-jQuery('.quantity').each(function() {
-    var spinner = jQuery(this),
-        input = spinner.find('input[type="number"]'),
-        btnUp = spinner.find('.quantity-up'),
-        btnDown = spinner.find('.quantity-down'),
-        min = input.attr('min'),
-        max = input.attr('max');
-
-    btnUp.click(function() {
-        var oldValue = parseFloat(input.val());
-        if (oldValue >= max) {
-            var newVal = oldValue;
-        } else {
-            var newVal = oldValue + 1;
-        }
-        spinner.find("input").val(newVal);
-        spinner.find("input").trigger("change");
-    });
-
-    btnDown.click(function() {
-        var oldValue = parseFloat(input.val());
-        if (oldValue <= min) {
-            var newVal = oldValue;
-        } else {
-            var newVal = oldValue - 1;
-        }
-        spinner.find("input").val(newVal);
-        spinner.find("input").trigger("change");
-    });
-
 });
 
 $(document).ready(function() {
